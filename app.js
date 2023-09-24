4ec2280c78fd51b70336d0df78319d44bfc0210a
gregory1996 <grigoriskatrakazas@gmail.com>
2023-09-24 08:37:20 +0300

    wip: update style of page

--- app.js
+++ app.js
@@ -1,14 +1,34 @@
 var CVView = Backbone.View.extend({
     template: _.template(`
-        <div class="bg-gray-600 p-6 rounded shadow-md">
-            <h1 class="text-3xl mb-4">Greg</h1>
-            <p class="mb-4"><strong>Role:</strong> Software Developer</p>
-            <h2 class="text-2xl mb-4">Skills</h2>
-            <ul>
-                <li class="mb-2">JavaScript</li>
-                <li class="mb-2">Python</li>
-                <li class="mb-2">Backbone.js</li>
-            </ul>
+        <div class="bg-cover bg-center h-screen w-full flex items-center justify-center" style="background-image: url('assets/images/Emerald.png');">
+            <div class="bg-white p-6 rounded shadow-md w-3/4">
+                <header class="border-b-2 border-gray-200 mb-4 p-2">
+                    <h1 class="text-3xl">Header Section</h1>
+                </header>
+                <div class="flex">
+                    <aside class="bg-customBlack text-white w-1/4 p-4">
+                        <h2 class="text-xl mb-2">Sidebar</h2>
+                        <ul>
+                            <li class="mb-1">Item 1</li>
+                            <li class="mb-1">Item 2</li>
+                            <li class="mb-1">Item 3</li>
+                        </ul>
+                    </aside>
+                    <main class="w-3/4 p-4">
+                        <h2 class="text-2xl mb-4">Greg</h2>
+                        <p class="mb-4"><strong>Role:</strong> Software Developer</p>
+                        <h3 class="text-xl mb-4">Skills</h3>
+                        <ul>
+                            <li class="mb-2">JavaScript</li>
+                            <li class="mb-2">Python</li>
+                            <li class="mb-2">Backbone.js</li>
+                        </ul>
+                    </main>
+                </div>
+                <footer class="border-t-2 border-gray-200 mt-4 p-2">
+                    <p>Footer Section</p>
+                </footer>
+            </div>
         </div>
     `),
     render: function() {
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@ module.exports = {
   purge: ['./index.html', './app.js'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'customBlack': '#2c3133',
+      },
+    },
   },
   variants: {
     extend: {},
